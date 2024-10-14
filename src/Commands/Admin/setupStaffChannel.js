ea50263a1c2fd1ab5b72e0d4e5c5f0ab7eb2f5e5
Dead <82973571+DeadGolden0@users.noreply.github.com>
2024-10-14 15:51:51 +0200

    Update setupStaffChannel.js

--- src/Commands/Admin/setupStaffChannel.js
+++ src/Commands/Admin/setupStaffChannel.js
@@ -1,6 +1,6 @@
 const ServerConfig = require('@Database/schemas/ServerConfig');
 const { SlashCommandBuilder } = require('discord.js');
-const logger = require('@Helpers/logger');
+const logger = require('@Helpers/Logger');
 
 module.exports = {
   data: new SlashCommandBuilder()
